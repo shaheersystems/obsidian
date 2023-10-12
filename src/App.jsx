990c745a7b663f128f4fd98ef654aefc5831db09
shaheersystems <shaheer.imran1429@gmail.com>
2023-10-12 18:41:24 +0500

    applayout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import AppLayout from "./layouts/AppLayout";
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+import NewJob from "./pages/NewJob";
 function App() {
   return (
     <div>
-      <h1 className="text-4xl">Obsidian</h1>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/jobs/new-job" element={<NewJob />} />
+        </Routes>
+      </AppLayout>
     </div>
   );
 }
---
+++ src/pages/NewJob.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NewJob() {
+  return (
+    <div>
+      <h1>New Job</h1>
+      <p>This is the New Job page.</p>
+    </div>
+  );
+}
+
+export default NewJob;
